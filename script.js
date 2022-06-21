416d2345f3b7a33b8219398ed5207b39b2ea412c
thimee <timswaggs1@outlook.com>
2022-06-21 09:35:00 +0100

    Done

---
+++ script.js
@@ -0,0 +1,16 @@
+let myName = "Timilehin Adenuga";
+let height = "6.0";//in feet
+let country = "Nigeria";
+
+// Displays in Html file
+
+document.querySelector('.name').textContent = `Name: ${myName}`; 
+document.querySelector('.height').textContent = `Height: ${height}ft`;
+document.querySelector('.country').textContent = `Country: ${country}`
+
+
+// to display on console
+
+// console.log('Name: ' + myName);
+// console.log('Height: '+ height +'ft');
+// console.log('Country: ' + country);
